cee05549882b652638a347923b91bd4597edbf7d
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-05-31 21:31:24 -0300

    Explanation improved

--- script.js
+++ script.js
@@ -4,17 +4,18 @@
 
   //Game Settings
   let settings = {
-    backgroundColor : 'black',
+    backgroundColor : 'black', //Background color
     playerX : 10, //Player start position X
     playerY : 10, //Player start position Y
-    playerColor : 'green',
+    playerColor : 'green', //Snake color
     speedX : 0, //Player start speed X
     speedY : 0, //Player start speed X
-    gridSize : 20, //Grid size
-    tileCount : 20, //Tiles in game (game size = grids * tiles)
+    gridSize : 25, //Grid size
+    tileCount : 25, //Tiles in game (game size = grids * tiles)
+    borderSize : 2, //grid line size
     goalX : 15, //Goal start position X
     goalY : 15, //Goal start position Y
-    goalColor : 'red',
+    goalColor : 'red', //Goal color
     trail : [], //Snake body array
     tail : 5, //Snake min length
     fps : 10 //Game frames per second
@@ -22,11 +23,13 @@
 
   //Defines elements and functions
   window.onload = function() {
+      //Define elements
       gameScreen = document.getElementById("game-screen");
       gameScreen.width = settings.gridSize * settings.tileCount;
       gameScreen.height = settings.gridSize * settings.tileCount;
       ctx = gameScreen.getContext("2d");
-      document.addEventListener("keydown", keyPush);
+      //Run
+      document.addEventListener("keydown", control);
       setInterval(game, 1000/settings.fps);
   }
 
@@ -50,26 +53,35 @@
         settings.playerY = 0;
       }
 
-      //Print background
+      //Print background on scene
       ctx.fillStyle = settings.backgroundColor;
       ctx.fillRect(0, 0, gameScreen.width, gameScreen.height);
+
+      //Print goal on scene
+      ctx.fillStyle = settings.goalColor;
+      ctx.fillRect(
+        settings.goalX * settings.gridSize,
+        settings.goalY * settings.gridSize,
+        settings.gridSize - borderSize, 
+        settings.gridSize - borderSize
+      );
    
-      //Print Snake
+      //Print Snake on scene
       ctx.fillStyle = settings.playerColor;
       for(let i = 0; i < settings.trail.length; i++) {
           ctx.fillRect(
             settings.trail[i].x * settings.gridSize,
             settings.trail[i].y * settings.gridSize,
-            settings.gridSize - 2,
-            settings.gridSize - 2
+            settings.gridSize - borderSize,
+            settings.gridSize - borderSize
           );
-          //Check self collision
+          //Check self collision (Game over)
           if (settings.trail[i].x === settings.playerX && settings.trail[i].y === settings.playerY) {
             settings.tail = 5;
           }
       }
 
-      //Move Snake
+      //Move Snake (print new head and exclude last tail)
       settings.trail.push({x: settings.playerX , y: settings.playerY});
       while(settings.trail.length > settings.tail) {
         settings.trail.shift();
@@ -81,29 +93,20 @@
         settings.goalX = Math.floor(Math.random() * settings.tileCount);
         settings.goalY = Math.floor(Math.random() * settings.tileCount);
       }
-
-      //Print goal
-      ctx.fillStyle = settings.goalColor;
-      ctx.fillRect(
-        settings.goalX * settings.gridSize,
-        settings.goalY * settings.gridSize,
-        settings.gridSize - 2, 
-        settings.gridSize - 2
-      );
   }
 
   //Controls
-  function keyPush(evt) {
-    if (evt.keyCode === 37) {
+  function control(evt) {
+    if (evt.keyCode === 37) { //up
       settings.speedX = -1;
       settings.speedY = 0;
-    } else if (evt.keyCode === 38) {
+    } else if (evt.keyCode === 38) { //left
       settings.speedX = 0;
       settings.speedY = -1;
-    } else if (evt.keyCode === 39) {
+    } else if (evt.keyCode === 39) { //down
       settings.speedX = 1;
       settings.speedY = 0;
-    } else if (evt.keyCode === 40) {
+    } else if (evt.keyCode === 40) { //right
       settings.speedX = 0;
       settings.speedY = 1;
     }
